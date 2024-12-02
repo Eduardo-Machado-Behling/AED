1eee696e8282d78675571400dea182d50dbd5e29
Eduardo Machado Behling <eduardomachadobehling@gmail.com7>
2024-12-02 13:36:12 -0300

    Found a better solution to the "Jump Game" medium problem

--- leetcode/medium/Jump Game/src/main.c
+++ leetcode/medium/Jump Game/src/main.c
@@ -3,51 +3,57 @@
 #include "solution.h"
 #include <stdlib.h>
 
-void setUp( void ) {
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void Test1(void) {
+  int a[] = {2, 3, 1, 1, 4};
+  int sa = sizeof(a) / sizeof(int);
+
+  const bool expected = true;
+  const bool res = canJump(a, sa);
+
+  TEST_ASSERT_EQUAL_INT(expected, res);
 }
 
-void tearDown( void ) {
+void Test2(void) {
+  int a[] = {3, 2, 1, 0, 4};
+  int sa = sizeof(a) / sizeof(int);
+
+  const bool expected = false;
+  const bool res = canJump(a, sa);
+
+  TEST_ASSERT_EQUAL_INT(expected, res);
 }
 
-void Test1( void ) {
-    int a[] = { 2, 3, 1, 1, 4 };
-    int sa  = sizeof( a ) / sizeof( int );
+void Test3(void) {
+  int a[] = {2, 0, 0};
+  int sa = sizeof(a) / sizeof(int);
 
-    const bool expected = true;
+  const bool expected = true;
+  const bool res = canJump(a, sa);
 
-    const bool res = canJump( a, sa );
-
-    TEST_ASSERT_EQUAL_INT( expected, res );
+  TEST_ASSERT_EQUAL_INT(expected, res);
 }
 
-void Test2( void ) {
-    int a[] = { 3, 2, 1, 0, 4 };
-    int sa  = sizeof( a ) / sizeof( int );
+void Test4(void) {
+  int a[] = {0};
+  int sa = sizeof(a) / sizeof(int);
 
-    const bool expected = false;
+  const bool expected = true;
+  const bool res = canJump(a, sa);
 
-    const bool res = canJump( a, sa );
-
-    TEST_ASSERT_EQUAL_INT( expected, res );
+  TEST_ASSERT_EQUAL_INT(expected, res);
 }
 
-void Test3( void ) {
-    int a[] = { 2, 0, 0 };
-    int sa  = sizeof( a ) / sizeof( int );
+int main(void) {
+  UNITY_BEGIN();
 
-    const bool expected = true;
+  RUN_TEST(Test1);
+  RUN_TEST(Test2);
+  RUN_TEST(Test3);
+  RUN_TEST(Test4);
 
-    const bool res = canJump( a, sa );
-
-    TEST_ASSERT_EQUAL_INT( expected, res );
-}
-
-int main( void ) {
-    UNITY_BEGIN();
-
-    RUN_TEST( Test1 );
-    RUN_TEST( Test2 );
-    RUN_TEST( Test3 );
-
-    return UNITY_END();
+  return UNITY_END();
 }
--- leetcode/medium/Jump Game/src/solution.c
+++ leetcode/medium/Jump Game/src/solution.c
@@ -6,37 +6,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NUMS_MAX_SIZE 10000
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
-enum state_t {
-    State_UNKOWN,
-    State_TRUE,
-    State_FALSE
-};
+// Did it with some outside help with the intuition
+bool canJump(const int *nums, int numsSize) {
+  int reachable = nums[0];
 
-bool Traverse_r( uint8_t* cache, const int* nums, const int index, int numsSize ) {
-    if ( cache[index] != State_UNKOWN ) {
-        return cache[index] == State_TRUE;
-    }
+  for (int i = 0; i <= reachable && reachable < numsSize; i++) {
+    reachable = MAX(reachable, i + nums[i]);
+  }
 
-    int jumps = nums[index];
-    if ( index == numsSize - 1 ) {
-        cache[index] = State_TRUE;
-        return true;
-    }
-
-    for ( int i = 1; index + i < numsSize && i <= jumps; i++ ) {
-        if ( Traverse_r( cache, nums, index + i, numsSize ) ) {
-            return true;
-        }
-    }
-
-    cache[index] = State_FALSE;
-    return false;
-}
-
-bool canJump( const int* nums, int numsSize ) {
-    // Yes, I'm caching, I don't know the magical O(n) solution YET.
-    uint8_t cache[NUMS_MAX_SIZE] = { 0 };
-    return Traverse_r( cache, nums, 0, numsSize );
+  return reachable >= numsSize - 1;
 }
