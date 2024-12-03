52c23d5fc90f5ff20efef17703f5708068c2c014
Eduardo Machado Behling <eduardomachadobehling@gmail.com7>
2024-12-03 14:47:06 -0300

    Solved the 3rd day puzzle of advent of code

---
+++ advent of code/day 3/src/part2.c
@@ -0,0 +1,78 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+  FILE *in = fopen("input.txt", "r");
+  if (!in) {
+    fprintf(stderr, "input file wasn't open for the following reason: \"%s\"\n",
+            strerror(errno));
+    return 1;
+  }
+
+  int digitCount = 0;
+  int currNumber = 0;
+
+  int i = 0;
+  const char fmt[] = "mul(,)";
+  int j = 0;
+  const char fmtEnabler[] = "don't()";
+  int type = 0;
+
+  char digitStr[2][4] = {0};
+  long res = 0;
+
+  int ch = 0;
+  int enable = 1;
+  while ((ch = fgetc(in)) != EOF) {
+    if (fmtEnabler[j] == '\0') {
+      enable = type;
+      j = 0;
+      type = 0;
+    }
+
+    if (fmtEnabler[j] == ch) {
+      j++;
+    } else if (j == 2 && ch == '(') {
+      j = 6;
+      type = 1;
+    } else if (j) {
+      j = 0;
+      type = 0;
+    }
+
+    if (fmt[i] == '\0') {
+      if (enable) {
+        res += strtol(digitStr[0], NULL, 10) * strtol(digitStr[1], NULL, 10);
+      }
+      i = 0;
+      digitCount = 0;
+      currNumber = 0;
+    }
+
+    if (i == 4 || i == 5) {
+      if (ch == ',' || ch == ')') {
+        i++;
+        digitStr[currNumber++][digitCount] = '\0';
+        digitCount = 0;
+        continue;
+      }
+      if (!isdigit(ch) || digitCount > 3) {
+        i = 0;
+        digitCount = 0;
+        currNumber = 0;
+        continue;
+      }
+      digitStr[currNumber][digitCount++] = (char)ch;
+    } else if (fmt[i] == ch) {
+      i++;
+    } else if (i) {
+      i = 0;
+    }
+  }
+
+  printf("res: %ld\n", res);
+  fclose(in);
+}
