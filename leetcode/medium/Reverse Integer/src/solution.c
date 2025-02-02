7893e08a91b96944258424ab79975770067e0bf5
Eduardo Machado Behling <embehling@inf.ufpel.edu.br>
2025-02-02 01:52:02 -0300

    A more honest (using int64_t seems like cheating) solution to the previously problem

--- leetcode/medium/Reverse Integer/src/solution.c
+++ leetcode/medium/Reverse Integer/src/solution.c
@@ -2,15 +2,28 @@
 
 #include <limits.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 int reverse( int x ) {
-    int64_t num = 0;
+    if ( x == INT32_MIN ) {
+        return 0;
+    }
+
+    const int threshold = INT32_MAX / 10;
+
+    int num  = 0;
+    int sign = x > 0 ? 1 : -1;
+    x        = abs( x );
 
     for ( int i = 0; x; i++ ) {
+        if ( num > threshold ) {
+            return 0;
+        }
+
         num *= 10;
         num += x % 10;
         x /= 10;
     }
 
-    return num > INT32_MAX || num < INT32_MIN ? 0 : num;
+    return num * sign;
 }
