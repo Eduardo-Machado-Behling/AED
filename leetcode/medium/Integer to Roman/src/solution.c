9c21cdb97c50d6f93a8c1d622bdf8d6d7adc459a
Eduardo Machado Behling <embehling@inf.ufpel.edu.br>
2025-02-02 03:17:54 -0300

    Found a better solution using a reasoning I saw online

--- leetcode/medium/Integer to Roman/src/solution.c
+++ leetcode/medium/Integer to Roman/src/solution.c
@@ -5,41 +5,23 @@
 
 #define BUFFERSIZE 1024
 
+// Better solution than mine, didn't thought of doing it like this.
 char* intToRoman( int num ) {
-    const char SYMBOLS[] = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+    const char* SYMBOLS[]      = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    const int SYMBOLS_VALUES[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
     char buffer[BUFFERSIZE] = { 0 };
-    int bufferIndex         = sizeof( buffer ) - 2;
+    int bufferI             = 0;
 
-    for ( int i = 2; num; i += 2 ) {
-        int digit = num % 10;
-        if ( digit <= 3 ) {
-            for ( size_t j = 0; j < digit; j++ ) {
-                buffer[bufferIndex--] = SYMBOLS[i - 2];
-            }
-        } else {
-            if ( digit <= 5 ) {
-                buffer[bufferIndex--] = SYMBOLS[i - 1];
-                for ( size_t j = 0; j < 5 - digit; j++ ) {
-                    buffer[bufferIndex--] = SYMBOLS[i - 2];
-                }
-            } else if ( digit <= 8 ) {
-                for ( size_t j = 0; j < digit - 5; j++ ) {
-                    buffer[bufferIndex--] = SYMBOLS[i - 2];
-                }
-                buffer[bufferIndex--] = SYMBOLS[i - 1];
-            } else {
-                buffer[bufferIndex--] = SYMBOLS[i];
-                for ( size_t j = 0; j < digit - 8; j++ ) {
-                    buffer[bufferIndex--] = SYMBOLS[i - 2];
-                }
+    for ( int i = 0; num; i += 1 ) {
+        for ( ; num >= SYMBOLS_VALUES[i]; num -= SYMBOLS_VALUES[i] ) {
+            for ( char* symbolStr = SYMBOLS[i]; *symbolStr; symbolStr++ ) {
+                buffer[bufferI++] = *symbolStr;
             }
         }
-
-        num /= 10;
     }
 
-    char* str = malloc( sizeof( buffer ) - bufferIndex );
-    strcpy( str, buffer + bufferIndex + 1 );
+    char* str = malloc( bufferI + 1 );
+    strcpy( str, buffer );
     return str;
 }
